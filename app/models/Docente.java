205df450c266556c9155443d105a71dc540c5a85
Luis Zárate <luis.zarate@solvosoft.com>
2016-05-25 11:11:15 -0600

    commit Docente

---
+++ app/models/Docente.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import org.springframework.ui.Model;
+import play.data.validation.Constraints;
+import java.util.*;
+import javax.persistence.*;
+
+import com.avaje.ebean.Model;
+import play.data.format.*;
+import play.data.validation.*;
+
+/**
+ *
+ * @author usuario
+ */
+@Entity
+public class Docente extends Model{
+    @Id
+    @Constraints.Min(10)
+    public long id;
+
+    @Constraints.Required
+    public String nombre;
+     
+     @Constraints.Required
+    public String materia;
+     
+      @Constraints.Required
+    public String cedula;
+ 
+        @Constraints.Required
+    public String telefono;
+
+    
+    
+    public static Finder<Long, Docente> find = new Finder<Long, Docente>(Docente.class); 
+    
+}
