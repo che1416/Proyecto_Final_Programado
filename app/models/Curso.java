fff77c1a167b9ef344b708a0a83241040aa07758
Luis Zárate <luis.zarate@solvosoft.com>
2016-05-25 11:48:45 -0600

    Merge origin/master
    Conflicts:
    	app/Filters.java
    
    Objecto estudiante mas long
    Completado el Objecto Curso

--- app/models/Curso.java
+++ app/models/Curso.java
@@ -5,10 +5,26 @@
  */
 package models;
 
+import java.util.ArrayList;
+import play.data.validation.Constraints;
+
 /**
  *
  * @author usuario
  */
-public class Curso {
+public class Curso extends Model {
     
+    
+    @Id
+    @Constraints.Min(10)
+    public long id;
+
+    @Constraints.Required
+    public String identificador;
+    
+    
+    ArrayList<Grupo> arrayGrupo = new ArrayList<Grupo>();
+     ArrayList<Docente> arrayDocente = new ArrayList<Docente>();
+    
+    public static Finder<Long, Curso> find = new Finder<Long, Curso>(Curso.class); 
 }
